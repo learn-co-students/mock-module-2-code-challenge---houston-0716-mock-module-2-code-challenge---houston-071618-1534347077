0e7989e483d87a6c73b96adbcb664d94cdbee83d
github username <github email address>
2018-08-15 11:35:19 -0500

    mock challenge

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,40 @@
 class EmployeesController < ApplicationController
-end
+    before_action :current_employee, only: [:show, :edit, :update]
+
+    def index
+      @employees = Employee.all
+    end
+
+    def show
+
+    end
+
+    def new
+      @employee = Employee.new
+    end
+
+    def create
+      @employee = Employee.create(employee_params)
+
+      redirect_to employee_path(@employee)
+    end
+
+    def edit
+
+    end
+
+    def update
+      current_employee.update(employee_params)
+
+      redirect_to employee_path(current_employee)
+    end
+
+    private
+    def employee_params
+      params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
+
+    def current_employee
+      @employee = Employee.find(params[:id])
+    end
+  end
