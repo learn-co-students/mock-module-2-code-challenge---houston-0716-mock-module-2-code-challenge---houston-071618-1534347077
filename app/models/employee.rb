28ce11756aaab9bb1382e777d4d2bd492b640b95
Christopher Arnold <chrisarnold2010@gmail.com>
2018-08-15 11:57:15 -0500

    done

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+	belongs_to :dog
+
+	validates :title, uniqueness: true
+	validates :alias, uniqueness: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+	
   resources :dogs
   resources :employees
+
+  # PATCH '/employees/:id', to: 'employees#update'
+  # Patch '/dogs/:id', to: 'dogs#update'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
