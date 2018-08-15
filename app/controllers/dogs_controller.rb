9a1615d305eb6678bff4bd28518c7e68b6874819
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-08-15 11:57:51 -0500

    turning in mock code challenge

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,44 @@
 class DogsController < ApplicationController
+  before_action :dog_params
+  helper_method :selected_dog
+  def show
+    @dog = selected_dog
+  end
+
+  def create
+    dog = Dog.create(dog_params)
+    redirect_to dog
+  end
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def edit
+    @dog = selected_dog
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    dog = Dog.create(dog_params)
+
+    redirect_to dog
+  end
+
+  def update
+  end
+
+  def selected_dog
+    Dog.find(params[:id])
+  end
+
+  private
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'dogs/show'
+
+  get 'dogs/index'
+
+  get 'dogs/edit'
+
+  get 'dogs/new'
+  
+  get 'employees/show'
+
+  get 'employees/index'
+
+  get 'employees/edit'
+
+  get 'employees/new'
+
   resources :dogs
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
