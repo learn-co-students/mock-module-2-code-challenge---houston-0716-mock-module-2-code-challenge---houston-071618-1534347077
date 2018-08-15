6642c454ca37ee7ee5de791d041d735b75369711
yendai-vo <yendai.vo@hotmail.com>
2018-08-15 11:57:30 -0500

    did not finish create and edit

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,43 @@
 class DogsController < ApplicationController
+  helper_method :selected_dog
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+
+    
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    dog = Dog.create(dog_params)
+
+    redirect_to dog_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    selected_dog.update(dog_params)
+
+    redirect_to dog_path(selected_dog)
+  end
+
+  def selected_dog
+    Dog.find(params[:id])
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
 end
