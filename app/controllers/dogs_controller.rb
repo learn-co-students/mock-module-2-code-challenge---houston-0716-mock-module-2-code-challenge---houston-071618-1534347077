e05f1d54ecec5b32989ce32a5daccfc3157fa136
Flatiron School <flatironschool@Flatirons-MacBook-Air-3.local>
2018-08-15 11:56:57 -0500

    commit

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,39 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = find_dog
+  end
+
+  def new
+
+  end
+
+  def edit
+    @dog = find_dog
+  end
+
+  def patch
+    byebug
+
+  end
+
+  def destroy
+    @dog = find_dog
+    @dog.destroy
+
+    redirect_to dog_path
+  end
+
+
+  private
+
+  def find_dog
+    Dog.find(params[:id])
+  end
+
+
 end
