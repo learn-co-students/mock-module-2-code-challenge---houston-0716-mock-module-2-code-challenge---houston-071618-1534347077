548efc4f3088b704a6ca53da766f84191dc7d6ea
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-08-15 11:35:08 -0500

    maybe DONE

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,30 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+
+    @dog.update(dog_params)
+
+    redirect_to @dog
+  end
+
+
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :age, :breed)
+  end
+
 end
