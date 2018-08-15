9952e0a7cea2e13d0adbedb2b2d81163e4692218
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-08-15 11:57:26 -0500

    init

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,28 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee =  Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    redirect_to employee_path(@employee)
+  end
+
+  def update
+  end
+
+private
+  def employee_params
+    params.require(:employee_params).permit(:first_name, :last_name, :alias, :title, :office)
+  end
+
 end
