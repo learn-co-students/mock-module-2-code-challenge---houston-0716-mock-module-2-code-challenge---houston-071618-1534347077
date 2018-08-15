e812cb9ee83fb7713cc482031db54acdfabc0a35
Scott Tang <fq600@yahoo.com>
2018-08-15 11:57:36 -0500

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,42 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def edit
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.save
+  end
+
+  def update
+    @dog.update(dog_params)
+  end
+
+  def destroy
+    @dog.destroy
+
+    redirect_to dogs_url
+  end
+
+  private
+
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,45 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+  end
+
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def edit
+  end
+
+  def create
+    # byebug
+    @employee = Employee.new(employee_params)
+    @employee.save
+    redirect_to employees_path
+  end
+
+  def update
+    @employee.update(employee_params)
+    redirect_to employees_url
+  end
+
+  def destroy
+    @employee.destroy
+    redirect_to employees_url
+  end
+
+  private
+
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+  end
 end
