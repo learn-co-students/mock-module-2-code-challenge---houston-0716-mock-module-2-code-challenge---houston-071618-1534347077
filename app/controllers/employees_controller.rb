961b3908a0cfcae00611dacec65256215237a9fb
github username <github email address>
2018-08-15 11:59:13 -0500

    mock code challenge 2

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,33 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = employee.find(params[:id])
+  end
+
+  def new
+    Employee.new
+  end
+
+  def create
+    employee = Employee.create(employee_params)
+
+    redirect_to employee
+  end
+
+
+  def edit
+  end
+
+  def delete
+  end
+
+  private
+   def employee_params
+     params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+   end
+
 end
