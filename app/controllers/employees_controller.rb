11171363b56cd2231c7f1c911e7fa5bfa099d560
kevinptx <kevin.peery@gmail.com>
2018-08-16 07:25:41 -0500

    updated solution

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,33 +1,46 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee
+
   def index
     @employees = Employee.all
   end
 
   def create
-    employee = Employee.create(employee_params)
-    redirect_to employees_path
+    employee = Employee.new(employee_params)
+    if employee.valid?
+      employee.save
+      redirect_to employees_path
+    else
+      redirect_to new_employee_path
+    end
   end
 
   def show
-    @employee = Employee.find(params[:id])
   end
 
   def edit
-    @employee = Employee.find(params[:id])
+    @dog_options = Dog.all.map { |dog| [dog.name, dog.id] }
   end
 
   def new
-    @employee = Employee.new
+    @dog_options = Dog.all.map { |dog| [dog.name, dog.id] }
   end
 
   def update
-    @employee = Employee.find(params[:id])
     @employee.update(employee_params)
-    @employee.save
     redirect_to employee_path(@employee)
   end
 
   private
+
+  def set_employee
+    if (params[:id] != nil)
+      # unless(params[:id] == nil)
+      @employee = Employee.find(params[:id])
+    else
+      @employee = Employee.new
+    end
+  end
 
   def employee_params
     params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
